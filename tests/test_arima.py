f9364c54e0d0c91a1cbf3562773e8628f9f76b89
Víctor Vázquez <victorvazrod@gmail.com>
2020-04-02 00:01:04 +0200

    Add models unit tests

---
+++ tests/test_arima.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append("src")
+
+import arima as model
+import unittest
+from unittest.mock import MagicMock
+
+class TestARIMA(unittest.TestCase):
+
+    def setUp(self):
+        self.mock = MagicMock()
+
+        self.model = model.ARIMA(
+            model_temp=self.mock,
+            model_hum=self.mock,
+        )
+
+    def test_predict_24hours(self):
+        self.mock.predict.return_value = [[x for x in range(24)],]
+
+        fc = self.model.predict(24)
+
+        self.assertEqual(len(fc), 24)
+
+    def test_predict_48hours(self):
+        self.mock.predict.return_value = [[x for x in range(48)],]
+
+        fc = self.model.predict(48)
+
+        self.assertEqual(len(fc), 48)
+
+    def test_predict_72hours(self):
+        self.mock.predict.return_value = [[x for x in range(72)],]
+
+        fc = self.model.predict(72)
+
+        self.assertEqual(len(fc), 72)
---
+++ tests/test_autoreg.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append("src")
+
+import autoreg as model
+import unittest
+from unittest.mock import MagicMock
+
+class TestARIMA(unittest.TestCase):
+
+    def setUp(self):
+        self.mock = MagicMock()
+
+        self.model = model.AutoReg(
+            model_temp=self.mock,
+            model_hum=self.mock,
+        )
+
+    def test_predict_24hours(self):
+        self.mock.predict.return_value = {x:x*2 for x in range(24, 48)}
+
+        fc = self.model.predict(24)
+
+        self.assertEqual(len(fc), 24)
+
+    def test_predict_48hours(self):
+        self.mock.predict.return_value = {x:x*2 for x in range(24, 72)}
+
+        fc = self.model.predict(48)
+
+        self.assertEqual(len(fc), 48)
+
+    def test_predict_72hours(self):
+        self.mock.predict.return_value = {x:x*2 for x in range(24, 96)}
+
+        fc = self.model.predict(72)
+
+        self.assertEqual(len(fc), 72)
